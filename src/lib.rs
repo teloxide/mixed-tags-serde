47748904d58450935bdeb4b5faa012037d1fcdde
Mr-Andersen <andrew-anderson.neo@yandex.ru>
2019-12-02 03:00:30 +0300

    Yet: delete all #[tagged] from enum variants

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,40 @@
 
 extern crate proc_macro;
 
-use syn::{DeriveInput, Item, parse_macro_input};
+use syn::{
+    DeriveInput, Expr,
+    ItemEnum, parse_macro_input,
+    punctuated::Punctuated, Token
+};
 use quote::{quote, ToTokens};
 
 use proc_macro::TokenStream;
 use proc_macro2;
 
 #[proc_macro_attribute]
-pub fn mixed_tags(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = Item::from(parse_macro_input!(item as DeriveInput));
-    let target = if let Item::Enum(target) = input {
-        target
-    } else {
-        return TokenStream::from(quote! {
-            compile_error!("SerdeHack works only with enums");
-        });
-    };
-    TokenStream::from(target.into_token_stream())
+pub fn mixed_tags(_attrs: TokenStream, item: TokenStream) -> TokenStream {
+    let mut item = parse_macro_input!(item as ItemEnum);
+
+    let filter = |attr: &syn::Attribute|
+        /*attr.style == syn::AttrStyle::Outer
+        &&*/
+        attr.path.segments.len() == 1
+        && attr.path.segments
+                .first().unwrap()
+                .ident.to_string()
+            == "tagged";
+    let _variants = item.variants.iter_mut()
+        .filter_map(|var|
+            if !var.attrs.iter()
+                .any(filter)
+            {
+                None
+            } else {
+                var.attrs.retain(|attr| !filter(attr));
+                Some(var)
+            }
+        )
+        .collect::<Vec<_>>();
+
+    item.into_token_stream().into()
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,9 +1,20 @@
-use serde::Serialize;
+// #[allow(unused_imports)]
+// use serde::Serialize;
+/*#![feature(trace_macros)]
 
-#[mixed_tag_serde::mixed_tags]
-#[derive(Serialize)]
+trace_macros!(true);*/
+
+#[allow(dead_code)]
+#[mixed_tag_serde::mixed_tags(Serialize, Deserialize)]
+#[derive(serde::Serialize)]
 enum MyEnum {
+    #[serde(rename = "one")]
+    #[tagged]
     One(i32),
+
+    #[serde(rename = "two")]
+    #[tagged]
     Two(u32),
+
     Three { x: i32, y: i32 }
 }
